ca2244f77bd249efdaaa067dfe63ee80d08612e0
unknown <nathanipaarth@gmail.com>
2024-06-08 14:18:37 -0700

    started work on training

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const endpoints = ['/', '/test'];
-for (const endpoint of endpoints) {
+const nav = fs.readFileSync(path.join(__dirname, 'public/nav.html'), 'utf8');
+app.get('/', (req, res) => {
+	res.send(
+		fs
+			.readFileSync(path.join(__dirname, 'public/index.html'), 'utf-8')
+			.replaceAll('{{nav}}', nav)
+	);
+});
+for (const endpoint of ['/training/intro*', '/training/phishing*']) {
 	app.get(endpoint, (req, res) => {
-		res.sendFile(path.join(__dirname, 'public', endpoint, 'index.html'));
+		const n = +path.basename(req.url);
+		res.send(
+			fs
+				.readFileSync(
+					path.join(
+						__dirname,
+						'public',
+						endpoint.replaceAll('*', ''),
+						(isNaN(n) ? 'index' : 'p' + n) + '.html'
+					),
+					'utf-8'
+				)
+				.replaceAll('{{nav}}', nav)
+		);
 	});
 }
-// app.get('/src/*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, req.url));
-// });
+app.get('/training', (req, res) => res.redirect('/training/intro'));
 app.use(express.static('dist'));
+app.use(express.static('./public/font'));
 
 app.listen(8080);
